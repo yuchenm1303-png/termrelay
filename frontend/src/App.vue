b08b4694118226dd0c566cd32f3264f68b6cb68c
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-07 23:31:03 +0800

    feat(account): mount workspace account dropdown

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
+import HomeAccountMenu from './smirel/components/HomeAccountMenu.vue'
 import WorkspaceShell from './smirel/components/WorkspaceShell.vue'
 
 const route = useRoute()
@@ -9,9 +10,12 @@ const useWorkspace = computed(() => route.meta.shell === 'workspace')
 
 <template>
   <RouterView v-slot="{ Component }">
-    <WorkspaceShell v-if="useWorkspace">
-      <component :is="Component" />
-    </WorkspaceShell>
+    <template v-if="useWorkspace">
+      <WorkspaceShell>
+        <component :is="Component" />
+      </WorkspaceShell>
+      <HomeAccountMenu variant="workspace" />
+    </template>
     <component :is="Component" v-else />
   </RouterView>
 </template>
