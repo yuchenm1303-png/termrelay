67ca50652f0f0dc7b6406487e76487d5f121f78b
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-17 17:17:58 +0800

    feat(ui): animate route changes inside stable workspace shell

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,12 +12,16 @@ const useWorkspace = computed(() => route.meta.shell === 'workspace')
   <RouterView v-slot="{ Component }">
     <template v-if="useWorkspace">
       <WorkspaceShell>
-        <component :is="Component" />
+        <Transition name="workspace-route" mode="out-in">
+          <component :is="Component" :key="route.path" />
+        </Transition>
       </WorkspaceShell>
       <Teleport to=".workspace-topbar-actions">
         <HomeAccountMenu variant="workspace" />
       </Teleport>
     </template>
-    <component :is="Component" v-else />
+    <Transition v-else name="app-route" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </Transition>
   </RouterView>
 </template>
